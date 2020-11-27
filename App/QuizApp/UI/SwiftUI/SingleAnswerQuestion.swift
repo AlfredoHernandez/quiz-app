eab8b5d045e499a4a6e1ce0a8be9911fd24f0513
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-11-26 19:37:50 -0600

    Add `SignleAnwerQuestion` view

---
+++ App/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -0,0 +1,61 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import SwiftUI
+
+struct SingleAnswerQuestion: View {
+    let title: String
+    let question: String
+    let options: [String]
+    let selection: (String) -> Void
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0.0) {
+            VStack(alignment: .leading, spacing: 16) {
+                Text(title)
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundColor(.blue)
+                    .padding(.top)
+                Text(question)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+            }.padding()
+            ForEach(options, id: \.self) { option in
+                Button(action: {}) {
+                    HStack {
+                        Circle()
+                            .stroke(Color.secondary, lineWidth: 2.5)
+                            .frame(width: 40, height: 40)
+                        Text(option)
+                            .font(.title)
+                            .foregroundColor(.secondary)
+                        Spacer()
+                    }.padding()
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+struct SingleAnswerQuestion_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SingleAnswerQuestion(
+                title: "1 of 2",
+                question: "What is Alfredo's nationality?",
+                options: ["Canadian", "Mexican", "Greek", "British"],
+                selection: { _ in }
+            )
+            SingleAnswerQuestion(
+                title: "1 of 2",
+                question: "What is Alfredo's nationality?",
+                options: ["Canadian", "Mexican", "Greek", "British"],
+                selection: { _ in }
+            ).preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .extraLarge)
+        }
+    }
+}
