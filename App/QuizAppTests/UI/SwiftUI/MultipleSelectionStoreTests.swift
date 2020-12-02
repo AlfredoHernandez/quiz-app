c22622da21e5099d5f7ff8ca06fcdcbc33bea1d5
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-01 23:35:16 -0600

    Move `MultipleSelectionStore` to production

--- App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
+++ App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -2,36 +2,8 @@
 //  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
 //
 
+@testable import QuizApp
 import XCTest
-
-struct MultipleSelectionStore {
-    private let handler: ([String]) -> Void
-    var options: [MultipleSelectionOption]
-    var canSubmit: Bool {
-        !options.filter(\.isSelected).isEmpty
-    }
-
-    init(options: [String], handler: @escaping ([String]) -> Void = { _ in }) {
-        self.options = options.map {
-            MultipleSelectionOption(text: $0)
-        }
-        self.handler = handler
-    }
-
-    func submit() {
-        guard canSubmit else { return }
-        handler(options.filter(\.isSelected).map(\.text))
-    }
-}
-
-struct MultipleSelectionOption {
-    let text: String
-    var isSelected = false
-
-    mutating func select() {
-        isSelected.toggle()
-    }
-}
 
 final class MultipleSelectionStoreTests: XCTestCase {
     func test_selectOption_togglesState() {
