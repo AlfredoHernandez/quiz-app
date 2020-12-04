54724039cc55d0bfd1eb63bd0be8f2afc43184c3
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-03 20:37:31 -0600

    Restart quiz when user taps 'Play Again'

--- App/QuizApp/AppDelegate.swift
+++ App/QuizApp/AppDelegate.swift
@@ -9,8 +9,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var quiz: Quiz?
+    private lazy var navigationController = UINavigationController()
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        startNewQuiz()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    private func startNewQuiz() {
         let question1 = Question.singleAnswer("What's Alfredo's nationality?")
         let question2 = Question.multipleAnswer("What are my favorite programming languages?")
         let questions = [question1, question2]
@@ -28,16 +38,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let options = [question1: options1, question2: options2]
         let correctAnswers = [(question1, [option2]), (question2, [option4, option6])]
 
-        let navigationController = UINavigationController()
-        let factory = iOSSwiftUIViewControllerFactory(options: options, correctAnswers: correctAnswers, playAgain: {})
+        let factory = iOSSwiftUIViewControllerFactory(options: options, correctAnswers: correctAnswers, playAgain: startNewQuiz)
         let router = NavigationControllerRouter(navigationController, factory: factory)
 
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-
         quiz = Quiz.start(questions: questions, delegate: router)
-
-        return true
     }
 }
